b2e28109b15de4f6fef21e3c001613165311aa45
Katerina Phamova <k.phamova@gmail.com>
2025-11-09 16:05:09 +0100

    Login_Invalid_feedback

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,31 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+//test('has title', async ({ page }) => {
+  //await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+ // await expect(page).toHaveTitle(/Playwright/);
+//});
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+//test('get started link', async ({ page }) => {
+ // await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  //await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+//});
+
+test('navigates to Czechitas',async ({page}) => {
+  await page.goto('/prihlaseni');
+
+  await page.getByRole('textbox', {name: 'Email'}).fill("blabla.pokus@czechitas.cz")
+  await page.getByRole('textbox', {name: 'Heslo'}).fill("password")
+
+  await page.getByRole('button', {name: 'Přihlásit'}).click()
+
+  await expect(page.locator(".invalid-feedback > strong:nth-child(1)")).toBeVisible()
+
 });
